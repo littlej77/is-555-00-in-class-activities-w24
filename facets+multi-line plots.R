3ec605cc12b4839b3d786b73a242881c08ac8336
littlej77 <jordantempest@gmail.com>
2024-02-05 13:06:15 -0700

    Update facets+multi-line plots.R

--- facets+multi-line plots.R
+++ facets+multi-line plots.R
@@ -13,15 +13,20 @@
 #       Note: Pay attention to the `scales` parameter in these facet functions, 
 #             where you can specify whether you want to hold the x and/or y
 #             axis scales constant across all of the facets.
+library(tidyverse)
 
 
 # Facets and multi-lines --------------------------------------------------------------------------------
-
 tips <- read_csv('https://www.dropbox.com/s/rydxlxdarjdoj7a/tips.csv?dl=1')
 
 # Let's plot tip_percentage vs. total_bill,
 # then split that across lots of categories
 
+tips %>% 
+  mutate(tip_perc = tip/total_bill)%>%
+  ggplot(aes(x=total_bill,y=tip_perc, color=as.factor(sex), alpha=sex, size=day))+ #to make it treat size as a cateogry do this as.factor() or as.character()
+  geom_point(size= 3)+
+  facet_wrap(~smoker) #facet_grid is better for doing multiple variables. can define row by nrow=3, ncol=3
 
 
 
@@ -29,3 +34,12 @@ econ <- read_csv('https://www.dropbox.com/s/8bq9rw0rk46hru2/econ.csv?dl=1')
 
 # Let's plot two measures over time: savings rate & unemployment weeks
 # It's easiest if we pivot to make this work
+
+econ %>% 
+  select(date, savings_rate, unempl_weeks) %>% 
+  filter(date<mdy('01-01-1975')) %>% 
+  pivot_longer(!date, names_to = "Measure", values_to = 'Rate') %>% 
+  ggplot(aes(x=date, y=Rate, color=Measure)) +
+  geom_line(size=1.5)+
+  facet_wrap(~Measure, ncol=1)
+
