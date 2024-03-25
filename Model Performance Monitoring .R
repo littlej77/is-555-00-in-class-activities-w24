627bde9ede2db23c883d136b7e7108f90243a068
littlej77 <jordantempest@gmail.com>
2024-03-25 15:13:02 -0600

    Update Model Performance Monitoring .R
    in class

--- Model Performance Monitoring .R
+++ Model Performance Monitoring .R
@@ -34,17 +34,39 @@ final_model <- rentals_wkfl %>%
   last_fit(split = rentals_split)
 
 # Prep for deployment:
+#we get the trained workflow out
+trained_workflow <- final_model %>% extract_workflow()
+#make a deployable model
+deployable_model <- vetiver_model(trained_workflow, 'rental_pricing_model')
 
 
 # Write the (deployable) vetiver model object to a pin board:
 
+pin_board <- board_folder('~/Desktop/section2')
 
+
+vetiver_pin_write(pin_board, deployable_model)
 
 # Now let's set ourselves up to do some performance monitoring. First we need 
 # some metrics that represent "expected" performance based on our test set used
 # during training.
 
+trained_workflow %>% 
+  predict(new_data = rentals_testing)
 
+#basically the same thing as this
+final_model %>%  collect_predictions()
+
+#show us the metrics over time. from our testing set. this is our baseline.
+chart_data <- final_model %>% collect_predictions() %>% 
+  bind_cols(rentals_testing %>% select(date_listed)) %>% 
+  vetiver_compute_metrics(date_var= date_listed,
+                          period='month',
+                          truth= sale_price,
+                          estimate= .pred)
+
+#vetiver is our MLOps package. this function takes our predictions and generates some baseline metrics
+vetiver_compute_metrics()
 
 # Let's look at performance over time during the training window. This requires
 # generating predictions from the training window. Which data should we use?
@@ -53,12 +75,19 @@ final_model <- rentals_wkfl %>%
 
 # The metrics are intentionally arranged for easy plotting. Create a line chart
 # that summarizes performance over time.
+chart_data %>% 
+  ggplot(aes(x=.index, y=.estimate, color=.metric)) +
+  geom_line()+
+  geom_point()+
+  facet_wrap(~.metric, scales= 'free_y',ncol = 1)
 
-
-
+#shortcut function
+chart_data %>% 
+  vetiver_plot_metrics()
 
 # Metrics are designed to be pinned alongside models for later referencing:
-
+#write this as a set of metrics to this board
+pin_board %>% pin_write(chart_data, 'rental_metrics')
 
 # Now let's roll the clock forward...
 # 
@@ -81,18 +110,37 @@ final_model <- rentals_wkfl %>%
 # Read in new data:
 new_rental_listings <- read_csv('https://www.dropbox.com/scl/fi/sgehr76gch8tux9pzdgfz/new_housing_with_dates.csv?rlkey=do0s9jbrgb2y53e728zrxl6zo&dl=1')
 
+new_rental_listings %>% select(date_listed) %>% summary()
+
 # Read in model from the pin board:
+
+pin_board 
+
+ old_model_object <- vetiver_pin_read(pin_board, 'rental_pricing_model')
 
 
 # Calculate performance metrics on new data
 
+new_metrics <- old_model_object %>% 
+  predict(new_data= new_rental_listings) %>% 
+  bind_cols(new_rental_listings %>% select(sale_price, date_listed)) %>% 
+  vetiver_compute_metrics(date_var=date_listed,
+                          period = 'month',
+                          truth= sale_price,
+                          estimate=.pred)
 
+new_metrics %>% 
+  vetiver_plot_metrics()
 
 # Update metrics (on the pin_board) so that we can use them next time:
 
-
+pin_board %>% vetiver_pin_metrics(df_metrics= new_metrics, 
+                                  metrics_pin_name = 'rental metrics')
 
 # Reusable plot from the pin_board "stash":
+pin_board %>% 
+  pin_read('rental_metrics') %>% 
+  vetiver_plot_metrics()
 
 
 
